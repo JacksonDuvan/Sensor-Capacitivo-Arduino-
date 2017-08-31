827c1873e4a1e974e52ba7c502ee0541e96dbb0f
Electron <31048230+JacksonDuvan@users.noreply.github.com>
2017-08-31 12:39:43 -0500

    Add files via upload

---
+++ ejemplo_fruta.ino
@@ -0,0 +1,22 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor sensor = CapacitiveSensor(4,3); //Declaramos una variable y los pines de arduino
+
+int led = 2; //Pin del led
+long estado = 0;
+int pulsado = LOW;//Comienza en estado bajo 
+void setup() {
+  pinMode(led,OUTPUT);
+}
+void loop() {
+
+  estado = sensor.capacitiveSensor(30);//Declaramos velocidad 
+
+  if (estado == HIGH){ //Condicional en alto 
+    pulsado = !pulsado;
+    digitalWrite(led,pulsado);
+    delay(500);
+    
+    }
+  
+}
